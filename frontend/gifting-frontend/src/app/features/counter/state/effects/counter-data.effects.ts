ca44a04d0e2a361435e80eef14fc98060bb0e18b
Caitlin Sheppeard <caitlin_s_sheppeard@progressive.com>
2022-12-15 15:59:36 -0500

    3pm thursday update

--- frontend/gifting-frontend/src/app/features/counter/state/effects/counter-data.effects.ts
+++ frontend/gifting-frontend/src/app/features/counter/state/effects/counter-data.effects.ts
@@ -3,10 +3,14 @@ import { Actions, createEffect, ofType } from "@ngrx/effects";
 import { ConsoleReporter } from "jasmine";
 import { tap } from "rxjs";
 import { CounterCommands } from "..actions/count-actions";
+import { CountState, initialState } from '../reducers/count-reducer';
+import { z } from 'zod';
 
 @Injectable()
 export class CounterDataEffects {
-    constructor(parameters) {
-
-    }
+    private readonly COUNT_DATA_KEY = 'count-data';
+    private readonly CountDataSchema = z.object({
+        current: z.number(),
+        by: z.union([z.literal(1)])
+    })
 }                                                                                                                                        
