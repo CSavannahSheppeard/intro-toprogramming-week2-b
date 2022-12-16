ec49dea7c48e4a33e16c23e5df9244793c1ec8e2
Caitlin Sheppeard <caitlin_s_sheppeard@progressive.com>
2022-12-16 09:42:45 -0500

    Done with Thursday

---
+++ backend/GiftingSolution/GiftingApi/Models/DeveloperModels.cs
@@ -0,0 +1,8 @@
+﻿namespace GiftingApi.Models;
+
+public record OnCallDeveloperResponse(string Name, string Email);
+
+/* {
+    "name": "Caitlin Sheppeard",
+    "email": "caitlin_s_sheppeard@progressive.com"
+}*/
